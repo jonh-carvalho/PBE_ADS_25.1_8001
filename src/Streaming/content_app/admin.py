6dfce080dd8ddd24f0a24de0bf8b8c0d6c4311be
Jonh Carvalho <jonh.carvalho@professores.ibmec.edu.br>
2025-05-23 16:02:12 -0300

    update streaming

--- src/Streaming/content_app/admin.py
+++ src/Streaming/content_app/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
-from content_app import models
+from content_app.models import Content, Playlist
 
-admin.site.register(models.Content)
-admin.site.register(models.Playlist)
+
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description','content_type', 'is_public', 'upload_date')
+    list_filter = ('content_type', 'is_public', 'upload_date')
+    search_fields = ('title', 'description')
+    ordering = ['upload_date']
+    #fields = ('title', 'description', 'file_url', 'thumbnail_url', 'creator')
+    fieldsets = (
+    ('Informações Básicas', {'fields': ('title', 'description')}),
+    ('Detalhes do Arquivo', {'fields': ('file_url', 'thumbnail_url')}),
+    ('Informações do Criador', {'fields': ('creator',)}),
+    ('Configurações', {'fields': ('content_type', 'is_public', 'status')}),
+)
+
+
+admin.site.register(Content, ContentAdmin)
+admin.site.register(Playlist)
 
